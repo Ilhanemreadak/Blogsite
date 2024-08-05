c4e3f23c7669e82e20e23518f894773e5771f63a
Ilhanemreadak <adak.ie@hotmail.com>
2024-08-05 09:56:04 +0300

    Added image upload services

--- Blog.Entity/Entities/Image.cs
+++ Blog.Entity/Entities/Image.cs
@@ -1,9 +1,5 @@
 ﻿using Blog.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Blog.Entity.Enums;
 
 namespace Blog.Entity.Entities
 {
---
+++ Blog.Service/Helpers/Images/IImageHelper.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Entity.Enums;
+using Blog.Entity.ViewModels.Images;
+using Microsoft.AspNetCore.Http;
+
+namespace Blog.Service.Helpers.Images
+{
+    public interface IImageHelper
+    {
+        Task<VMImageUploded> Upload(string name, IFormFile imageFile, ImageType imageType, string folderName = null);
+        void Delete(string imageName);
+    }
+}
