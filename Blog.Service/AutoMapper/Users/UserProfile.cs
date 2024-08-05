3167bdc318df242f5b677b20f2afb4a0a4dfb8ac
Ilhanemreadak <adak.ie@hotmail.com>
2024-08-05 17:48:29 +0300

    Added user datatable and view

---
+++ Blog.Service/AutoMapper/Users/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Blog.Entity.Entities;
+using Blog.Entity.ViewModels.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service.AutoMapper.Users
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        { 
+            CreateMap<AppUser, VMUser>().ReverseMap();
+        }
+    }
+}
